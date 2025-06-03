f190f6ac3f4412c71d38b4f7a8a4c77a7cfbadba
Maratbalov <maratbalov888@gmail.com>
2025-06-03 14:07:14 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>ERP запущена ✅</h1>
+      <p>Скоро тут появится таблица остатков и AI-помощник</p>
+    </div>
+  )
+}
